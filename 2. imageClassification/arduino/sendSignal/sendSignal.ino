404126fdab9b81a27892b332481921698e578238
Jason Juhyun Kim <mtinet@hanmail.net>
2022-01-30 23:11:54 +0900

    Add files via upload

---
+++ 2. imageClassification/arduino/sendSignal/sendSignal.ino
@@ -0,0 +1,12 @@
+int output_data = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println('s');
+  delay(3000); // delay 500ms
+}
--- 2. imageClassification/imageClassificatorLabels.py
+++ 2. imageClassification/imageClassificatorLabels.py
@@ -5,6 +5,24 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from PIL import ImageFont, ImageDraw, Image
 
+import serial
+import serial.tools.list_ports
+import time
+
+ports = serial.tools.list_ports.comports()
+com = ''
+
+# 아두이노 연결 com 포트 찾아서 자동으로 연결하기
+for port, desc, hwid in sorted(ports):
+    if 'Arduino' in desc:
+        com = port
+if com != '':
+    print('\n arduino USB detected: ', com)
+else:
+    print('\nPlease connect your microbit to this PC via USB')
+
+ser = serial.Serial(com, 9600, timeout=0)
+
 # 모델 가져오기
 model = load_model('model.h5')
 model.summary()
@@ -77,7 +95,19 @@ while webcam.isOpened():
     # display
     cv2.imshow('imageClassficator', frame)
 
+    # 시리얼 통신으로 's'가 들어오면 추론 결과를 보여줌
+    if ser.readable():
+        val = ser.readline()
+        if (len(val) > 0) :
+            val = val.decode()[:len(val)-1]  # 넘어온 데이터 중 마지막 개행문자 제외
+            # print(val)
+            # 아두이노에서 'i'가 입력되면 숫자를 올리고, 'o'가 입력되면 숫자를 내림, 0보다 작아지면 0으로 고정함.
+            if ('s' in val) :
+                print(np.round(prediction[0], 2))
+                print(predicted_class)
 
+
+    # press "s" to print predicted_class
     if cv2.waitKey(1) & 0xFF == ord('s'):
         print(np.round(prediction[0], 2))
         print(predicted_class)
